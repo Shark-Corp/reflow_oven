c7b7b27e3f821b7c39770e2061e0bafcb5ba341d
Adrien Luitot <23219818+adrienluitot@users.noreply.github.com>
2023-11-19 11:48:15 +0100

    init

---
+++ reflow_oven.ino
@@ -0,0 +1,14 @@
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+   digitalWrite(2, HIGH);  //LED PIN SET HIGH
+    delay(1000);              // 1 SEC DELAY
+    digitalWrite(2, LOW);   //LED PIN SET LOW
+    delay(1000); 
+}
