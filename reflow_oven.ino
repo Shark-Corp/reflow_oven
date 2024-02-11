59051dbcc3dbc4084744cf4346d00f2c597780ad
axelbalda <axelbalda1709@gmail.com>
2024-02-11 21:33:45 +0100

    ESP led test .ino update + .gitignore updated

--- reflow_oven.ino
+++ reflow_oven.ino
@@ -2,13 +2,13 @@
 
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(2, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
    digitalWrite(2, HIGH);  //LED PIN SET HIGH
-    delay(1000);              // 1 SEC DELAY
+    delay(100);              // 1 SEC DELAY
     digitalWrite(2, LOW);   //LED PIN SET LOW
-    delay(1000); 
+    delay(100); 
 }
